7ef43628f1ff04d7e01a4427398f221ed5ddab43
KIM-YONG-HA <vb901217@gmail.com>
2025-02-21 14:18:09 +0900

    250221 CNN

--- 정보처리기사/실기/clang/hello.c
+++ 정보처리기사/실기/clang/hello.c
@@ -2,10 +2,22 @@
 int main(){
     
 
-    char* p = "KOREA";
-    printf("%s\n ", p);
-    printf("%s\n ", p+1);
-    printf("%c\n ", *p);
-    printf("%c\n ", *(p+3));
-    printf("%c\n ", *p+4);
+    // char* p = "KOREA";
+    // printf("%s\n ", p);
+    // printf("%s\n ", p+1);
+    // printf("%c\n ", *p);
+    // printf("%c\n ", *(p+3));
+    // printf("%c\n ", *p+4);
+
+    // int a = 10;
+    // printf("aï¿½ï¿½ ï¿½ï¿½: %d\n", a);   // ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ ï¿½ï¿½ï¿?
+    // printf("aï¿½ï¿½ ï¿½Ö¼ï¿½: %p\n", &a); // ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½Ö¼ï¿½ ï¿½ï¿½ï¿?
+
+
+    int a = 10;
+
+    printf("a °ª : %d\n", a);
+    printf("a address value : %d\n", &a);
+
+
 }
